ac7473b2f7f5fc5bbd1fe42a5e3ef7e5acae84cb
VladislavBodikov <krullerr@yandex.ru>
2022-08-29 20:10:14 +0300

    dependencies and introduction

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # lumiere-project-server-part
 Lumiere is a project that allows users to share their interests, hobbies and recommendations with other users.
+
+---
+This product uses the TMDB API but is not endorsed or certified by TMDB 
+![](D:\JavaPrograms\lumiere-project-server-part\src\main\resources\TMDB.png)
---
+++ src/main/java/com/lumiere/LumiereApplication.java
@@ -0,0 +1,11 @@
+package com.lumiere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumiereApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LumiereApplication.class, args);
+    }
+}
